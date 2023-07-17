6a5aa942bd72f08889f679de9286f01f16da5780
yolobi <dimdimdimas@apps.ipb.ac.id>
2023-07-17 09:24:17 +0700

    get current user from request headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,27 @@
 class ApplicationController < ActionController::API
-  include Response
-  include ExceptionHandler
+    include Response
+    include ExceptionHandler
+
+    before_action :authorize_request
+    attr_reader :current_user
+
+    private
+
+    def authorize_request
+        @current_user = decode_jwt_token
+        render json: { error: 'Unauthorized' }, status: :unauthorized unless @current_user
+    end
+
+    def decode_jwt_token
+        authorization_header = request.headers['Authorization']
+        token = authorization_header.split(' ').last if authorization_header.present?
+
+        return unless token
+
+        decoded_token = JsonWebToken.decode(token)
+        user_id = decoded_token[:user_id]
+        User.find(user_id) if user_id
+    rescue JWT::DecodeError, JWT::VerificationError, JWT::ExpiredSignature
+        nil
+    end
 end
